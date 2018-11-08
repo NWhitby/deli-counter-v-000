dd16f1d2f987c1cfcc70d595d335935af055ea7e
NaKia Whitby <whitby.nakia@gmail.com>
2018-11-08 15:56:23 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,4 +8,7 @@ def line(katz_deli)
       message += " #{index.to_i+1}. #{value}"
     end
 
-    
+def take_a_number(katz_deli, name)
+  katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+end
